51d8b87aae294701640639616f0b7602f9297741
faatihat <tomfaah@gmail.com>
2025-06-04 07:24:44 +0100

    feat:test-landing-page

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: {
-      enabled: false,
-    },
+    turbo: false,
   },
   images: {
-    domains: ["images.pexels.com"],
-  },
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-   images: {
     domains: ['images.pexels.com'],
-  },
-  experimental: {
-    turbo: false,
   },
 };
 
