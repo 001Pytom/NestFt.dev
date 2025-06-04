3e2467153167f794f194b3f4af9924741f607eba
faatihat <tomfaah@gmail.com>
2025-06-04 07:11:32 +0100

    fix/next-error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      enabled: false,
+    },
+  },
+  images: {
+    domains: ["images.pexels.com"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
